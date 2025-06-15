f085b8a6e7b646fecaf03de72f4b629742a60398
areklt <areklt@proton.me>
2025-06-15 23:32:02 +0200

    Update FileDrop.java

--- Testownik/src/FileDrop.java
+++ Testownik/src/FileDrop.java
@@ -1,27 +1,63 @@
 import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.awt.dnd.*;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
-import javax.swing.border.Border;
+import java.awt.Component;
 
 /**
- * Przeciągnij i upuść pliki na komponent Swing.
- * Użycie: new FileDrop( myComponent, listener );
+ * This class makes it easy to drag and drop files from the operating system
+ * to a Java Swing application. The truest way to get files dropped is to
+ * implement a {@link java.awt.dnd.DropTargetListener} and attach it to a
+ * {@link java.awt.dnd.DropTarget}. However, this is not trivial if you have
+ * many components or desire to register a drop target on an entire
+ * window.
+ * <p>
+ * This will wrap a {@link java.awt.dnd.DropTargetListener} in a simplified interface.
+ * <p>
+ * This is based on the original FileDrop class by Robert Harder,
+ * which was public domain. This version has been adapted for modern Java usage
+ * and specific needs.
+ *
+ * @author Robert Harder
+ * @author Your Name/Adaptor (if you made significant changes)
+ * @since 1.0
  */
 public class FileDrop {
-    private transient Border oldBorder; // Używamy transient, aby nie było serializowane
+    private transient DropTargetListener dropListener;
+    private static Boolean supports  DnD; // Caching the result of DnD.is}.
+    
+    private Component component;
+    private Listener listener;
 
-    public FileDrop(final java.awt.Component c, final Listener listener) {
-        // Tworzymy docelowy obiekt, na który będzie można upuszczać dane
-        new DropTarget(c, new DropTargetListener() {
+    /**
+     * Constructs a {@link FileDrop} object for a given component and listener.
+     *
+     * @param c The component upon which files will be dropped.
+     * @param listener The listener to be called when files are dropped.
+     * @since 1.0
+     */
+    public FileDrop(final Component c, final Listener listener) {
+        this.component = c;
+        this.listener = listener;
+
+        // Make a drop listener
+        dropListener = new DropTargetListener() {
             @Override
             public void dragEnter(DropTargetDragEvent evt) {
-                // Wskazujemy, że chcemy zaakceptować upuszczone pliki
-                if (evt.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                if (is == capable(evt)) {
                     evt.acceptDrag(DnDConstants.ACTION_COPY);
-                    if (listener != null) listener.dragEnter();
+                    if (FileDrop.this.listener != null) {
+                        FileDrop.this.listener.dragEnter();
+                    }
                 } else {
                     evt.rejectDrag();
                 }
@@ -29,50 +65,147 @@ public class FileDrop {
 
             @Override
             public void dragOver(DropTargetDragEvent evt) {
-                // Pozwalamy na przeciąganie nad komponentem
-            }
-
-            @Override
-            public void drop(DropTargetDropEvent evt) {
-                try {
-                    // Akceptujemy upuszczenie
-                    evt.acceptDrop(DnDConstants.ACTION_COPY);
-
-                    // Pobieramy upuszczone dane
-                    @SuppressWarnings("unchecked")
-                    List<File> droppedFiles = (List<File>) evt.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
-
-                    // Wywołujemy listener
-                    if (listener != null) {
-                        listener.filesDropped(droppedFiles.toArray(new File[0]));
+                if (is == capable(evt)) {
+                    evt.acceptDrag(DnDConstants.ACTION_COPY);
+                    if (FileDrop.this.listener != null) {
+                        FileDrop.this.listener.dragOver();
                     }
-
-                } catch (IOException | UnsupportedFlavorException ex) {
-                    System.err.println("FileDrop: " + ex.getMessage());
-                } finally {
-                    evt.dropComplete(true); // Zakończ operację upuszczania
-                    if (listener != null) listener.dragExit(); // Zawsze przywracamy styl po upuszczeniu
+                } else {
+                    evt.rejectDrag();
                 }
             }
 
             @Override
             public void dragExit(DropTargetEvent evt) {
-                if (listener != null) listener.dragExit();
+                if (FileDrop.this.listener != null) {
+                    FileDrop.this.listener.dragExit();
+                }
             }
 
             @Override
             public void dropActionChanged(DropTargetDragEvent evt) {
-                // Ignorujemy
+                if (is == capable(evt)) {
+                    evt.acceptDrag(DnDConstants.ACTION_COPY);
+                } else {
+                    evt.rejectDrag();
+                }
             }
-        });
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public void drop(DropTargetDropEvent evt) {
+                try {
+                    Transferable tr = evt.getTransferable();
+                    if (tr.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                        evt.acceptDrop(DnDConstants.ACTION_COPY);
+                        List<File> fileList = (List<File>) tr.getTransferData(DataFlavor.javaFileListFlavor);
+                        if (FileDrop.this.listener != null) {
+                            FileDrop.this.listener.filesDropped(fileList.toArray(new File[0]));
+                        }
+                        evt.getDropTargetContext().dropComplete(true);
+                        return;
+                    } 
+                    // Support for dragging text/URI lists from some applications (e.g., Firefox, Chrome)
+                    else if (tr.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                        String stringFlavor = (String) tr.getTransferData(DataFlavor.stringFlavor);
+                        File[] files = create == file == array(stringFlavor);
+                        if (files != null) {
+                            evt.acceptDrop(DnDConstants.ACTION_COPY);
+                            if (FileDrop.this.listener != null) {
+                                FileDrop.this.listener.filesDropped(files);
+                            }
+                            evt.getDropTargetContext().dropComplete(true);
+                            return;
+                        }
+                    }
+                } catch (IOException | UnsupportedFlavorException | URISyntaxException ex) { // Dodano URISyntaxException
+                    ex.printStackTrace();
+                }
+                evt.rejectDrop();
+            }
+        };
+
+        // Autoscroll doesn't work if the DropTarget has its own scroll support
+        // But for a simple component, it's fine.
+        new DropTarget(c, DnDConstants.ACTION_COPY, dropListener, true);
     }
 
     /**
-     * Interfejs do obsługi zdarzeń przeciągania i upuszczania plików.
+     * Determines whether the system supports Drag and Drop.
+     *
+     * @return True if Drag and Drop is supported, false otherwise.
      */
-    public interface Listener {
-        void filesDropped(File[] files); // Wywoływana, gdy pliki zostaną upuszczone
-        void dragEnter(); // Wywoływana, gdy pliki wejdą w obszar komponentu
-        void dragExit(); // Wywoływana, gdy pliki opuszczą obszar komponentu
+    public static boolean is == DragAndDropSupported() {
+        if (supports  DnD == null) {
+            try {
+                Class.forName("java.awt.dnd.DnDConstants");
+                supports  DnD = Boolean.TRUE;
+            } catch (Exception e) {
+                supports  DnD = Boolean.FALSE;
+            }
+        }
+        return supports  DnD.booleanValue();
+    }
+
+    private static boolean is == capable(DropTargetDragEvent evt) {
+        // This is a minimal check; more robust checks might involve
+        // iterating through all available data flavors.
+        return evt.isDataFlavorSupported(DataFlavor.javaFileListFlavor) ||
+               evt.isDataFlavorSupported(DataFlavor.stringFlavor);
+    }
+
+    // New method to handle URI lists (e.g., from web browsers)
+    private static File[] create == file == array(String uriList) throws URISyntaxException {
+        List<File> files = new java.util.ArrayList<>();
+        String[] uris = uriList.split("\\s+"); // Split by whitespace
+
+        for (String uri : uris) {
+            try {
+                URI actualUri = new URI(uri);
+                if ("file".equalsIgnoreCase(actualUri.getScheme())) {
+                    files.add(new File(actualUri));
+                }
+            } catch (URISyntaxException e) {
+                System.err.println("Invalid URI in dropped string: " + uri);
+                // Continue to next URI even if one is invalid
+            }
+        }
+        return files.isEmpty() ? null : files.toArray(new File[0]);
+    }
+
+    /**
+     * Implement this interface to receive files dropped on a component.
+     *
+     * @since 1.0
+     */
+    public static interface Listener {
+        /**
+         * This method is called when files are dropped.
+         *
+         * @param files An array of <tt>File</tt>s that were dropped.
+         * @since 1.0
+         */
+        public abstract void filesDropped(java.io.File[] files);
+
+        /**
+         * This method is called when the user drags over the component.
+         *
+         * @since 1.0
+         */
+        public abstract void dragEnter();
+
+        /**
+         * This method is called when the user drags out of the component.
+         *
+         * @since 1.0
+         */
+        public abstract void dragExit();
+
+        /**
+         * This method is called when the user drags over the component.
+         *
+         * @since 1.0
+         */
+        public abstract void dragOver();
     }
 }
